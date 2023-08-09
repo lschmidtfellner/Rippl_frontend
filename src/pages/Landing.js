b350d047394d27426d9f794e13fed0a5a8fea0d5
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-08-09 18:49:35 -0400

    tailwind is sick

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Landing() {
   return (
     <div className="landing ">
-      <h1 className='font-bold'>GET RECCOMENDATIONS</h1>
+      <h1 className='font-forma-djr-micro font-extra-bold text-7xl break-words pt-14'>GET <br/>RECO- MEND- ATIONS</h1>
       <h3>(from Spotify)</h3>
       <Link to="/input">
         <button className="get-started">GET STARTED</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,20 @@ export const content = [
   "./src/**/*.{js,jsx,ts,tsx}",
 ];
 export const theme = {
-  extend: {},
+  extend: {
+    fontFamily: {
+      'forma-djr-micro': ['forma-djr-micro', 'sans-serif']
+    },
+    fontWeight: {
+      'regular': 400,
+      'medium': 500,
+      'bold' : 700,
+      'extra-bold': 800
+    },
+    fontStyle: {
+      'normal': 'normal',
+      'italic': 'italic'
+    }
+  },
 };
 export const plugins = [];
