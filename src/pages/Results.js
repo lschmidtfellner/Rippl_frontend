48eae7e709d3ed24cd7acd4a28747ff8c64ad057
lschmidtfellner <127900788+lschmidtfellner@users.noreply.github.com>
2023-08-20 22:38:41 -0400

    removed dropshadow from results imgs

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -28,13 +28,20 @@ function Results({ recommendations, setRecommendations }) {
     const lines = resultRef.current.children
 
     if (recommendations.length === 0) {
-      navigate('/');
+      navigate('/')
     }
 
     gsap.fromTo(
       lines,
       { opacity: 0, y: -10 },
-      { opacity: 1, y: 0, duration: 0.6, delay: 1, stagger: 0.2, ease: 'power2.out' }
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.6,
+        delay: 1,
+        stagger: 0.2,
+        ease: 'power2.out'
+      }
     )
   }, [navigate, recommendations])
 
@@ -50,56 +57,64 @@ function Results({ recommendations, setRecommendations }) {
 
   return (
     <div>
-      <ResultsBackground ref={backgroundRef}/>
-    <div ref={resultRef} className="pt-20 lg:px-20 2xl:px-48">
-      <h1 className="font-comba font-extra-bold text-5xl lg:text-8xl mt-8 mb-16">
-        HERE <br /> YOU GO!
-      </h1>
-      {recommendations.map((item, index) => (
-        <div
-          className={`preview-container mt-8 font-proxima border-black pb-10 w-full flex justify-center items-center ${
-            index !== recommendations.length - 1 ? 'border-b' : ''
-          }`}
-          key={item.spotify_id}
-        >
-          <img src={item.artURL} alt={item.artist} className="h-20 mr-6" style={{boxShadow: '-5px 5px 0px #345131'}} />
-          <div className="song-info text-lg whitespace-nowrap overflow-hidden truncate w-80">
-            {item.title.length > 18 ? (
-              <Marquee velocity={12} repeat={2}>
-                {[<p className="font-bold">{item.title}&emsp;~&emsp;{item.title}&emsp;~&emsp;</p>]}
-              </Marquee>
+      <ResultsBackground ref={backgroundRef} />
+      <div ref={resultRef} className="pt-20 lg:px-20 2xl:px-48">
+        <h1 className="font-comba font-extra-bold text-5xl lg:text-8xl mt-8 mb-16">
+          HERE <br /> YOU GO!
+        </h1>
+        {recommendations.map((item, index) => (
+          <div
+            className={`preview-container mt-8 font-proxima border-black pb-10 w-full flex justify-center items-center ${
+              index !== recommendations.length - 1 ? 'border-b' : ''
+            }`}
+            key={item.spotify_id}
+          >
+            <img src={item.artURL} alt={item.artist} className="h-20 mr-6" />
+            <div className="song-info text-lg whitespace-nowrap overflow-hidden truncate w-80">
+              {item.title.length > 18 ? (
+                <Marquee velocity={12} repeat={2}>
+                  {[
+                    <p className="font-bold">
+                      {item.title}&emsp;~&emsp;{item.title}&emsp;~&emsp;
+                    </p>
+                  ]}
+                </Marquee>
+              ) : (
+                <p className="song-name font-bold">{item.title}</p>
+              )}
+              {item.artist.length > 18 ? (
+                <Marquee velocity={12} repeat={2}>
+                  {[
+                    <p>
+                      {item.artist}&emsp;~&emsp;{item.artist}&emsp;~&emsp;
+                    </p>
+                  ]}
+                </Marquee>
+              ) : (
+                <p className="song-artist">{item.artist}</p>
+              )}
+            </div>
+            {item.previewURL ? (
+              <AudioPlayer
+                ref={(element) => (playerRefs.current[index] = element)}
+                className="ml-5"
+                src={item.previewURL}
+                volume={0.6}
+                onPlay={() => handlePlay(index)}
+                onPause={() =>
+                  currentPlayingIndex === index && setCurrentPlayingIndex(null)
+                }
+                showJumpControls={false}
+                customAdditionalcontrols={[]}
+              />
             ) : (
-              <p className="song-name font-bold">{item.title}</p>
-            )}
-            {item.artist.length > 18 ? (
-              <Marquee velocity={12} repeat={2}>
-                {[<p>{item.artist}&emsp;~&emsp;{item.artist}&emsp;~&emsp;</p>]}
-              </Marquee>
-            ) : (
-              <p className="song-artist">{item.artist}</p>
+              <div className="preview-container ml-4">
+                <p className="font-extra-bold">N/A</p>
+              </div>
             )}
           </div>
-          {item.previewURL ? (
-            <AudioPlayer
-              ref={(element) => (playerRefs.current[index] = element)}
-              className="ml-5"
-              src={item.previewURL}
-              volume={0.6}
-              onPlay={() => handlePlay(index)}
-              onPause={() =>
-                currentPlayingIndex === index && setCurrentPlayingIndex(null)
-              }
-              showJumpControls={false}
-              customAdditionalcontrols={[]}
-            />
-          ) : (
-            <div className="preview-container ml-4">
-              <p className="font-extra-bold">N/A</p>
-            </div>
-          )}
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
     </div>
   )
 }
